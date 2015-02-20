b97b7bfa2017bfd6165883eb827d765a2ecdbbe1
Travis Nesland <tnesland@gmail.com>
2015-02-20 10:48:25 -0500

    Bugfix: limit param needs to be converted to integer.

--- server/app/controllers/recipes_controller.rb
+++ server/app/controllers/recipes_controller.rb
@@ -13,7 +13,7 @@ class RecipesController < ApplicationController
   # Params:
   # :limit - how many random recipes to return (default 3)
   def random
-    limit = params[:limit] ||= 3
+    limit = params[:limit].to_i ||= 3
     @recipes = []
     limit.times do
       @recipes.push(Recipe.random_recipe)
@@ -25,10 +25,13 @@ class RecipesController < ApplicationController
   # Params:
   # :limit - how many random recipes to return (default 3)
   def search
-    limit = params[:limit] ||= 10
+    limit = params[:limit].to_i ||= 10
     @recipes = []
     limit.times do
       @recipes.push(Recipe.random_recipe)
     end
   end
+
+  private
+  
 end
